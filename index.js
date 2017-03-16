e025ec9f32e47e7b429f80f9d06edc7ef5ad1702
Poga Po <poga.bahamut@gmail.com>
2017-03-17 05:01:44 +0800

    add base timestamp to index header

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require('fs')
 const pb = require('protocol-buffers')
 const path = require('path')
+const uint64be = require('uint64be')
 
 const messages = pb(fs.readFileSync(path.join(__dirname, 'messages.proto')))
 
+const VERSION = 1
+const HEADER_SIZE = 14
 const INDEX_ITEM_SIZE = 15
+
 const DEFAULT_MAX_SEGMENT_SIZE = 5 * 1024 * 1024 // max segment size in bytes
 
 module.exports = RATS
@@ -30,7 +34,7 @@ function RATS (path, opts) {
       }
     })
     this.currentSegmentOffset = maxOffset
-    this.currentOffset = this.currentSegmentOffset + (fs.statSync(this.currentIndex()).size) / INDEX_ITEM_SIZE
+    this.currentOffset = this.currentSegmentOffset + (fs.statSync(this.currentIndex()).size - HEADER_SIZE) / INDEX_ITEM_SIZE
     this.currentSegmentSize = fs.statSync(this.currentLog()).size
   }
 }
@@ -38,14 +42,19 @@ function RATS (path, opts) {
 RATS.prototype.append = function (obj, time, cb) {
   if (!cb) {
     cb = time // time is optional
-    time = Date.now()
+    time = Math.round(Date.now() / 1000)
   }
   var data = new Buffer(JSON.stringify(Object.assign({}, obj, {time: time})) + '\n')
   var self = this
 
+  if (this.currentOffset === 0) {
+    appendHeader(time)
+  }
+
   if (this.currentSegmentSize >= this.maxSegmentSize) {
     this.currentSegmentOffset = this.currentOffset
     this.currentSegmentSize = 0
+    appendHeader(time)
   }
 
   appendLog(data, time)
@@ -67,6 +76,11 @@ RATS.prototype.append = function (obj, time, cb) {
 
       cb()
     })
+  }
+
+  function appendHeader (baseTimestamp) {
+    var header = {version: VERSION, baseTimestamp: uint64be.encode(baseTimestamp)}
+    fs.appendFileSync(self.currentIndex(), messages.Header.encode(header))
   }
 }
 
--- test/index.js
+++ test/index.js
@@ -5,6 +5,7 @@ var path = require('path')
 var rimraf = require('rimraf')
 var mkdirp = require('mkdirp')
 var messages = require('protocol-buffers')(fs.readFileSync(path.join(__dirname, '..', 'messages.proto')))
+var uint64be = require('uint64be')
 
 tape('append', function (t) {
   var dir = path.join('.', 'temp')
@@ -15,19 +16,35 @@ tape('append', function (t) {
 
   function test () {
     var rats = new RATS(dir)
-    rats.append({foo: 'bar'}, function (err) {
+    var now = Math.round(Date.now() / 1000)
+    rats.append({foo: 'bar'}, now, function (err) {
       t.error(err)
 
       var indexFile = path.join(dir, '0.index')
       var logFile = path.join(dir, '0.log')
+
+      // check index and log size
       var index = fs.statSync(indexFile)
-      t.equal(index.size, 15)
+      t.equal(index.size, 29, 'index file size')
       var log = fs.statSync(logFile)
-      t.equal(log.size, 35)
-      t.same(messages.Index.decode(fs.readFileSync(indexFile)), { offset: 0, position: 0, size: 35 })
+      t.equal(log.size, 32, 'log file size')
+
+      // check decode index header
+      var headerBuf = new Buffer(14)
+      fs.readSync(fs.openSync(indexFile, 'r'), headerBuf, 0, 14)
+      var header = messages.Header.decode(headerBuf)
+      t.equal(header.version, 1, 'decode header version')
+      t.same(header.baseTimestamp, uint64be.encode(now), 'decode header base timestamp')
+
+      // check decode index item
+      var indexBuf = new Buffer(15)
+      fs.readSync(fs.openSync(indexFile, 'r'), indexBuf, 0, 15, 14)
+      t.same(messages.Index.decode(indexBuf), { offset: 0, position: 0, size: 32 }, 'decode index item')
+
+      // check decode log
       var data = JSON.parse(fs.readFileSync(logFile))
-      t.same(data.foo, 'bar')
-      t.ok(data.time)
+      t.same(data.foo, 'bar', 'decode log')
+      t.ok(data.time, 'decode log time')
       rimraf(dir, function () {
         t.end()
       })
@@ -52,14 +69,29 @@ tape('segment', function (t) {
 
         var indexFile = path.join(dir, '1.index')
         var logFile = path.join(dir, '1.log')
+
+        // check index and log size
         var index = fs.statSync(indexFile)
-        t.equal(index.size, 15)
+        t.equal(index.size, 29, 'index size')
         var log = fs.statSync(logFile)
-        t.equal(log.size, 35)
-        t.same(messages.Index.decode(fs.readFileSync(indexFile)), { offset: 1, position: 0, size: 35 })
+        t.equal(log.size, 32, 'log size')
+
+        // check decode index header
+        var headerBuf = new Buffer(14)
+        fs.readSync(fs.openSync(indexFile, 'r'), headerBuf, 0, 14)
+        var header = messages.Header.decode(headerBuf)
+        t.equal(header.version, 1, 'decode header version')
+        t.ok(header.baseTimestamp, 'decode header base timestamp')
+
+        // check decode index item
+        var indexBuf = new Buffer(15)
+        fs.readSync(fs.openSync(indexFile, 'r'), indexBuf, 0, 15, 14)
+        t.same(messages.Index.decode(indexBuf), { offset: 1, position: 0, size: 32 }, 'decode index item')
+
+        // check decode log
         var data = JSON.parse(fs.readFileSync(logFile))
-        t.same(data.foo, 'baz')
-        t.ok(data.time)
+        t.same(data.foo, 'baz', 'decode log')
+        t.ok(data.time, 'decode log time')
 
         rimraf(dir, function () {
           t.end()
